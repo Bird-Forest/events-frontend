1517b58f4ad379810683aa6d0cb50f36e9d5fccd
Bird-Forest <bird.forest70@gmail.com>
2024-05-19 10:03:44 +0300

    url

--- src/redux/EventSlice.js
+++ src/redux/EventSlice.js
@@ -2,7 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const eventsApi = createApi({
   reducerPath: 'events',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/api' }),
+  // baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:4000/api' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://events-backend-ybfh.onrender.com',
+  }),
   tagTypes: ['Events'],
   endpoints: builder => ({
     getEventsAll: builder.query({
